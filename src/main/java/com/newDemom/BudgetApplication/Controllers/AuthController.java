8a73ddf6d5afb9efd3f4367d0cb5455f3a88a320
mckent05 <atemitope95@gmail.com>
2024-04-17 23:09:28 +0100

    Added cross origin method to Auth controller

--- src/main/java/com/newDemom/BudgetApplication/Controllers/AuthController.java
+++ src/main/java/com/newDemom/BudgetApplication/Controllers/AuthController.java
@@ -5,12 +5,10 @@ import com.newDemom.BudgetApplication.Domain.Dto.RegisterDto;
 import com.newDemom.BudgetApplication.Service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/auth")
 public class AuthController {
 
